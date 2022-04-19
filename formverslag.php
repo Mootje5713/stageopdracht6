7134c085ad1be5e095cdb7503c89e0ddced6bd17
Mohammed baallach <2113080@talnet.nl>
2022-04-19 12:41:00 +0200

    update

--- formverslag.php
+++ formverslag.php
@@ -2,7 +2,7 @@
 <form method="POST" action="">
     <label for="verslag">Wat heb je allemaal gedaan vandaag?</label>
     <br>
-    <textarea id="verslag" name="verslag" value="<?php echo $data['verslag']; ?>" rows="4" cols="50" maxlength="3000" required></textarea>
+    <textarea id="verslag" name="verslag" value="<?php var_dump($data['verslag']); ?>" rows="4" cols="50" maxlength="3000" required></textarea>
     <br>
     <input type="submit" class="btn" name="submit">
 </form>
