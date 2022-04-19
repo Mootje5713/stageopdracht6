ffab36ccf7c5f59a6c7b72c40f32bac321b932c5
Mohammed baallach <2113080@talnet.nl>
2022-04-19 14:02:51 +0200

    update

--- connection.php
+++ connection.php
@@ -15,16 +15,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 //echo $_SERVER['REQUEST_URI'];
-var_dump($_SERVER['REQUEST_URI']);
 if (!isset($_SESSION['user_id']) && $_SERVER['REQUEST_URI']!='/stageopdracht6/login.php'
 && $_SERVER['REQUEST_URI']!='/stageopdracht6/register.php') {
     header("Location: login.php");
 }
 
-if (strpos($mystring, $findme) !== false) {
+if (strpos($_SERVER['REQUEST_URI'], '_pb') !== false) {
     if (isset($_SESSION['praktijkbegeleider_user_id'])) {
         header("Location: index.php");
-        $mystring = $_SERVER['REQUEST_URI']; 
-        $findme = '_pb';
     }
 }
