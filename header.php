02a31e52e1e927821c4305ffbe2a5e6d05019542
Mohammed baallach <2113080@talnet.nl>
2022-05-03 15:09:28 +0200

    update

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./wicket/resource/assets.AssetsResourceReferenceMarker/stylesheets/styles-ver-791C75DF32208B3036D6915808D48FEE.css" />
     <title>Document</title>
 </head>
 <body>
